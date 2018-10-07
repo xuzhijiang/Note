5136ea825ad2a634c3a7ee1097dfaee050c6f589
ZhiJiangMichael <2233835996@qq.com>
2018-10-07 21:44:12 +0800

    update

--- JAVA/fundamental/src/org/java/core/base/multithreading/FutureTask/FutureTaskExample.java
+++ JAVA/fundamental/src/org/java/core/base/multithreading/FutureTask/FutureTaskExample.java
@@ -7,6 +7,15 @@ import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+/**
+ * FutureTask是Future接口的基本具体实现，并提供异步处理。
+ * 它包含启动和取消任务的方法，以及可以返回FutureTask状态的方法，
+ * 无论它是完成还是取消,
+ * 我们需要一个可调用对象来创建未来任务，然后我们可以使用Java线程池执行器来异步处理这些任务。
+ * <p><br>
+ * FutureTask没有任何好处，但是当我们想要覆盖一些Future接口方法
+ * 并且不想实现Future接口的每个方法时，它会派上用场。
+ */
 public class FutureTaskExample {
 	public static void main(String[] args) {
 		MyCallable callable1 = new MyCallable(1000);//同一个包中的MyCallable不用引入
@@ -30,10 +39,12 @@ public class FutureTaskExample {
 
 				if (!futureTask1.isDone()) {
 					// wait indefinitely for future task to complete
+					//get方法无限期等待未来的任务完成
 					System.out.println("FutureTask1 output=" + futureTask1.get());
 				}
 
 				System.out.println("Waiting for FutureTask2 to complete");
+				////get方法等待200毫秒
 				String s = futureTask2.get(200L, TimeUnit.MILLISECONDS);
 				if (s != null) {
 					System.out.println("FutureTask2 output=" + s);
--- JAVA/fundamental/src/org/java/core/base/multithreading/ScheduledThreadPoolExecutor/ScheduledThreadPool.java
+++ JAVA/fundamental/src/org/java/core/base/multithreading/ScheduledThreadPoolExecutor/ScheduledThreadPool.java
@@ -5,17 +5,41 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 有时我们需要定期执行任务或在特定延迟后执行任务。 
+ * Java提供了Timer Class，通过它我们可以实现这一点，
+ * 但有时我们需要并行运行类似的任务。 因此，创建多个Timer对象将是系统的开销，
+ * 并且最好有一个计划任务的线程池。
+ * <p><br>
+ * Java通过实现ScheduledExecutorService接口的
+ * ScheduledThreadPoolExecutor类提供预定的线程池实现.
+ * <p><br>
+ * 请注意，所有schedule（）方法都返回ScheduledFuture的实例，
+ * 我们可以使用它来获取线程的线程状态信息和延迟时间。
+ * <p><br>
+ * 时间段是从池中第一个线程的开始，
+ * 因此如果您将period指定为1秒并且您的线程运行5秒，
+ * 那么下一个线程将在第一个工作线程完成它的执行后立即开始执行。
+ */
 public class ScheduledThreadPool {
 
 	public static void main(String[] args) throws InterruptedException {
 		ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);
 
-		// schedule to run after sometime
 		System.out.println("Current Time = " + new Date());
 		for (int i = 0; i < 3; i++) {
 			Thread.sleep(1000);
 			WorkerThread worker = new WorkerThread("do heavy processing");
-			scheduledThreadPool.schedule(worker, 10, TimeUnit.SECONDS);
+			// schedule to run after sometime
+			scheduledThreadPool.schedule(worker, 5, TimeUnit.SECONDS);
+			
+			// schedule task to execute at fixed rate
+//			scheduledThreadPool.scheduleAtFixedRate(worker, 0, 5, TimeUnit.SECONDS);
+			
+			//ScheduledExecutorService scheduleWithFixedDelay方法
+			//可用于以初始延迟启动定期执行，然后以给定的延迟执行。 延迟时间是从线程完成执行的时间开始
+			scheduledThreadPool.scheduleWithFixedDelay(worker, 1, 4,
+					TimeUnit.SECONDS);
 		}
 
 		// add some delay to let some threads spawn by scheduler
