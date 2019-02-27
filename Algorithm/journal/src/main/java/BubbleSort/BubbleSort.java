2a6ba64c7843947751ecc696592bd5e5c728d0c9
XuZhiJiang <xuzhijiang@sunniwell.net>
2019-02-27 19:13:30 +0800

    update

--- Algorithm/journal/src/main/java/BubbleSort/BubbleSort.java
+++ Algorithm/journal/src/main/java/BubbleSort/BubbleSort.java
@@ -1,74 +1,43 @@
 package BubbleSort;
 
-// Optimized java implementation of Bubble sort
+import java.util.Arrays;
+
 public class BubbleSort {
 
-    // An unoptimized version of Bubble sort
-    static void bubbleSort(int[] list) {
-        for (int i = 0; i < list.length; i++) {
-            for (int j = i + 1; j < list.length; j++) {
-                if (list[i] > list[j]) {
-                    swap(list, i, j);
+    public static void main(String[] args){
+        int[] arr = new int[]{2, 1, 7, 9, 28, 23, 17, 100, 203};
+        System.out.println("排序之前: " + Arrays.toString(arr));
+        sort(arr, true);
+        System.out.println("由小到大结果: " + Arrays.toString(arr));
+        sort(arr, false);
+        System.out.println("由大到小结果: " + Arrays.toString(arr));
+    }
+
+    /**
+     *
+     * @param arr 是否是升序(由小到大)
+     * @param asc
+     */
+    private static void sort(int[] arr, boolean asc){
+        for(int i=0;i<arr.length;i++){//一共要排(arr.length - 1)轮
+            for(int j=i+1;j<arr.length;j++){
+                if(asc){//由小到大排列
+                    if(arr[i] > arr[j]){//如果是大在前，小在后，所以要交换成小在前，大在后
+                        swap(arr, i, j);//第一轮排序完后的结果是，最小的在0的位置
+                    }
+                }else{//由大到小
+                    if(arr[i] < arr[j]){
+                        swap(arr, i, j);
+                    }
                 }
             }
         }
     }
 
-    static void swap(int[] arr, int i, int j) {
-        int temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
+    private static void swap(int[] arr, int i, int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 
-    // An optimized version of Bubble Sort
-    static void bubbleSort(int[] arr, int n) {
-        int i, j, temp;
-        boolean swapped;
-        for (i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // swap arr[j] and arr[j+1]
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
-                }
-            }
-
-            // IF no two elements were
-            // swapped by inner loop, then break
-            if (swapped == false)
-                break;
-        }
-    }
-
-    // 在归并排序中用到的是冒泡排序的修改版，多了两个参数first和last，
-    // 只对数组中索引从first到last的元素进行冒泡排序
-    public static void BubbleSort(int[] list, int first, int last) {
-        for (int i = first; i <= last; i++) {
-            for (int j = i + 1; j <= last; j++) {
-                if (list[i] > list[j]) {
-                    swap(list, i, j);
-                }
-            }
-        }
-    }
-
-    // Function to print an array
-    static void printArray(int[] arr, int size) {
-        int i;
-        for (i = 0; i < size; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
-
-    // Driver program
-    public static void main(String[] args) {
-        int[] arr = {64, 34, 25, 12, 22, 11, 90};
-        int n = arr.length;
-        bubbleSort(arr, n);
-        System.out.println("Sorted array: ");
-        printArray(arr, n);
-    }
 }
